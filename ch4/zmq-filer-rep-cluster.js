fb3208b34db9483dbfbb0395e49793aaa045d2ff
Chris Ross <rosscj2533@gmail.com>
2015-01-04 10:56:02 -0500

    Clustering example

---
+++ ch4/zmq-filer-rep-cluster.js
@@ -0,0 +1,62 @@
+'use strict';
+const
+	cluster = require('cluster'),
+	fs = require('fs'),
+	zmq = require('zmq');
+	
+if (cluster.isMaster) {
+
+	// master process, create ROUTER and DEALER sockets, bind endpoints
+	let 
+		router = zmq.socket('router').bind('tcp://127.0.0.1:5433'),
+		dealer = zmq.socket('dealer').bind('tcp://127.0.0.1:5444');  // bound to different tcp port since ipc not supported on windows
+		//dealer = zmq.socket('dealer').bind('ipc://filer-dealer.ipc');
+	
+	// forward messages between router and dealer
+	router.on('message', function() {
+		let frames = Array.prototype.slice.call(arguments);
+		//let frames = Array.prototype.slice.call(null, arguments); // removed null
+		dealer.send(frames);
+	});
+	
+	dealer.on('message', function() {
+		let frames = Array.prototype.slice.call(arguments);
+		// let frames = Array.prototype.slice.call(null, arguments); // removed null
+		router.send(frames);
+	});
+
+	// listen for workers to come online
+	cluster.on('online', function(worker) {
+		console.log('Worker ' + worker.process.pid + ' is online.');
+	});
+	
+	// fork three worker processes
+	for (let i = 0; i < 3; i++) {
+		cluster.fork();
+	}
+	
+} else {
+
+	// worker process - create REP socket, connect to DEALER
+	let responder = zmq.socket('rep').connect('tcp://127.0.0.1:5444');
+	// let responder = zmq.socket('rep').connect('ipc://filer-dealer.ipc');
+	
+	responder.on('message', function(data) {
+	
+		// parse incoming message
+		let request = JSON.parse(data);
+		console.log(process.pid + ' received request for: ' + request.path);
+		
+		// read file and rely with content
+		fs.readFile(request.path, function(err, data) {
+			console.log(process.pid + ' sending response');
+			responder.send(JSON.stringify({
+				pid: process.pid,
+				data: data.toString(),
+				timestamp: Date.now()
+			}));
+		});
+		
+	});
+	
+}
