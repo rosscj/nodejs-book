ed08fc544d0d354f2feb903c32d0e3b42dc6fb08
Chris Ross <rosscj2533@gmail.com>
2015-01-25 08:09:15 -0500

    Fix up unit test, it's not perfect, but good enough to continue

--- ch5/databases/lib/rdf-parser.js
+++ ch5/databases/lib/rdf-parser.js
@@ -9,36 +9,22 @@ module.exports = function(filename, callback) {
 		let
 			$ = cheerio.load(data.toString()),
 			collect = function(index, elem) {
-				// console.log('hit collect with elem text = ' + $(elem).text());
+				// console.log('in collect, returning ' + $(elem).text());
 				return $(elem).text();
 			};
-		
-		// //console.log('pgterms\\:marc260 = ' + $('pgterms\\:marc260').text());
-		// // console.log("$('marcrel\\trl pgterms\\:agent pgterms\\:name') = " + $('marcrel\\:trl pgterms\\:agent pgterms\\:name').text());
-		// //console.log("$('pgterms\\:agent pgterms\\:name') = " + $('pgterms\\:agent pgterms\\:name').text());
-		// $('pgterms\\:agent pgterms\\:name').each(function (elm) {
-			// console.log("in each, elm = " + elm);
+			
+		// example of logging each element in collection
+		// $('pgterms\\:agent pgterms\\:name').each(function(index, elm) {
+			// console.log("subject - " + $(elm).text());
 		// });
-		
-		$('dcterms\\:subject rdf\\:Description rdf\\:value').each(function (index, elm) {
-			console.log("in each, elm = " + $(elm).text());
-		});
-		
-		 $('[rdf\\:resource$="/LCSH"]').each(function (index, elm) {
-			console.log("in 2nd each, elm = " + $(elm).text());
-		 });
-		
-		// console.log("subjects = " + $('dcterms\\:subject rdf\\:Description rdf\\:value').text());
-		
-		// console.log('author mapped = ' + $('marcrel\\:trl pgterms\\:agent pgterms\\:name').map(collect));
+
 		
 		callback(null, {
 			_id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
 			title: $('dcterms\\:title').text(),
 			authors: $('pgterms\\:agent pgterms\\:name').map(collect),
-			// authors: $('marcrel\\:trl pgterms\\:agent pgterms\\:name').map(collect).get().join(' '),
-			subjects: $('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(collect)
-			//subjects: $('dcterms\\:subject rdf\\:Description rdf\\:value').map(collect)
+			//subjects: $('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(collect) // original, but siblings and/or ends with not working...
+			subjects: $('dcterms\\:subject rdf\\:description rdf\\:value').map(collect)
 		});		
 	});
 };
--- ch5/databases/test/test-rdf-parser.js
+++ ch5/databases/test/test-rdf-parser.js
@@ -5,33 +5,26 @@ const
 	
 exports.testRDFParser = function(test) {
 	rdfParser(__dirname + '/pg132.rdf', function(err, book) {
-		test.expect(3); // count of how many asserts there will be
+		test.expect(10); // count of how many asserts there will be
 		test.ifError(err);
-		// console.log('id = ' + book._id);
-		// console.log('title = ' + book.title);
-		// console.log('authors = ' + book.authors);
-		// console.log('authors length = ' + book.authors.length);
-		// console.log('authors at first index = ' + book.authors[0]);
-		// console.log('subjects = ' + book.subjects);
-		// console.log('book = ' + book);
-		// console.log(book);
-		console.log('book authors length = ' + book.authors.length);
-		// this will never show anything as book.authors .... why??
-		console.log('book authors = ' + book.authors);
+
+		// // this will never show anything as book.authors .... why?? other things work, like typeof(book.authors), book.authors.length
+		// console.log('book authors = ' + book.authors);
 		
-		console.log('expected authors = ' + expectedValue.authors);
+		// deepEqual is not working, seems related to why book.authors console.log won't work...
+		// so do this more painful and fragile asserting...
+		// test.deepEqual(book, expectedValue, "book should match expected");
 		
-		console.log('book subjects length = ' + book.subjects.length);
-		// this will never show anything as book.authors .... why??
-		console.log('book subjects = ' + book.subjects);
-		
-		console.log('expected subjects = ' + expectedValue.subjects);
-		
-		//test.deepEqual(book, expectedValue, "book should match expected");
-		//test.equal(book, expectedValue);
 		test.equal(book._id, expectedValue._id);
 		test.equal(book.title, expectedValue.title);
-		// test.deepEqual(book.authors, expectedValue.authors);
+		test.equal(book.authors.length, expectedValue.authors.length);
+		test.equal(book.authors[0], expectedValue.authors[0]);
+		test.equal(book.authors[1], expectedValue.authors[1]);
+		test.equal(book.subjects.length, expectedValue.subjects.length);
+		test.equal(book.subjects[0], expectedValue.subjects[0]);
+		test.equal(book.subjects[1], expectedValue.subjects[1]);
+		test.equal(book.subjects[2], expectedValue.subjects[2]);
+		
 		test.done();
 	});
 };
