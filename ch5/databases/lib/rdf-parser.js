faf24bfb8b33553ffec8fd965a4367c897e8991e
Chris Ross <rosscj2533@gmail.com>
2015-01-24 22:29:09 -0500

    More debugging, getting closer

--- ch5/databases/lib/rdf-parser.js
+++ ch5/databases/lib/rdf-parser.js
@@ -9,24 +9,36 @@ module.exports = function(filename, callback) {
 		let
 			$ = cheerio.load(data.toString()),
 			collect = function(index, elem) {
-				console.log('hit collect with elem text = ' + $(elem).text());
+				// console.log('hit collect with elem text = ' + $(elem).text());
 				return $(elem).text();
 			};
 		
-		console.log('pgterms\\:marc260 = ' + $('pgterms\\:marc260').text());
-		// console.log("$('marcrel\\trl pgterms\\:agent pgterms\\:name') = " + $('marcrel\\:trl pgterms\\:agent pgterms\\:name').text());
-		console.log("$('pgterms\\:agent pgterms\\:name') = " + $('pgterms\\:agent pgterms\\:name').text());
-		$('pgterms\\:agent pgterms\\:name').each(function (elm) {
-			console.log("in each, elm = " + elm);
+		// //console.log('pgterms\\:marc260 = ' + $('pgterms\\:marc260').text());
+		// // console.log("$('marcrel\\trl pgterms\\:agent pgterms\\:name') = " + $('marcrel\\:trl pgterms\\:agent pgterms\\:name').text());
+		// //console.log("$('pgterms\\:agent pgterms\\:name') = " + $('pgterms\\:agent pgterms\\:name').text());
+		// $('pgterms\\:agent pgterms\\:name').each(function (elm) {
+			// console.log("in each, elm = " + elm);
+		// });
+		
+		$('dcterms\\:subject rdf\\:Description rdf\\:value').each(function (index, elm) {
+			console.log("in each, elm = " + $(elm).text());
 		});
 		
-		console.log('author mapped = ' + $('marcrel\\:trl pgterms\\:agent pgterms\\:name').map(collect));
+		 $('[rdf\\:resource$="/LCSH"]').each(function (index, elm) {
+			console.log("in 2nd each, elm = " + $(elm).text());
+		 });
+		
+		// console.log("subjects = " + $('dcterms\\:subject rdf\\:Description rdf\\:value').text());
+		
+		// console.log('author mapped = ' + $('marcrel\\:trl pgterms\\:agent pgterms\\:name').map(collect));
 		
 		callback(null, {
 			_id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
 			title: $('dcterms\\:title').text(),
-			authors: $('marcrel\\:trl pgterms\\:agent pgterms\\:name').map(collect),
+			authors: $('pgterms\\:agent pgterms\\:name').map(collect),
+			// authors: $('marcrel\\:trl pgterms\\:agent pgterms\\:name').map(collect).get().join(' '),
 			subjects: $('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(collect)
+			//subjects: $('dcterms\\:subject rdf\\:Description rdf\\:value').map(collect)
 		});		
 	});
 };
--- ch5/databases/test/test-rdf-parser.js
+++ ch5/databases/test/test-rdf-parser.js
@@ -5,15 +5,33 @@ const
 	
 exports.testRDFParser = function(test) {
 	rdfParser(__dirname + '/pg132.rdf', function(err, book) {
-		test.expect(1); // example says this should be 2.... don't know what it means...
+		test.expect(3); // count of how many asserts there will be
 		test.ifError(err);
-		console.log('id = ' + book._id);
-		console.log('title = ' + book.title);
-		console.log('authors = ' + book.authors);
-		console.log('subjects = ' + book.subjects);
-		console.log('book = ' + book);
+		// console.log('id = ' + book._id);
+		// console.log('title = ' + book.title);
+		// console.log('authors = ' + book.authors);
+		// console.log('authors length = ' + book.authors.length);
+		// console.log('authors at first index = ' + book.authors[0]);
+		// console.log('subjects = ' + book.subjects);
+		// console.log('book = ' + book);
 		// console.log(book);
+		console.log('book authors length = ' + book.authors.length);
+		// this will never show anything as book.authors .... why??
+		console.log('book authors = ' + book.authors);
+		
+		console.log('expected authors = ' + expectedValue.authors);
+		
+		console.log('book subjects length = ' + book.subjects.length);
+		// this will never show anything as book.authors .... why??
+		console.log('book subjects = ' + book.subjects);
+		
+		console.log('expected subjects = ' + expectedValue.subjects);
+		
 		//test.deepEqual(book, expectedValue, "book should match expected");
+		//test.equal(book, expectedValue);
+		test.equal(book._id, expectedValue._id);
+		test.equal(book.title, expectedValue.title);
+		// test.deepEqual(book.authors, expectedValue.authors);
 		test.done();
 	});
 };
