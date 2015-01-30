601ee05fdc193c0efdb7868911ac519ef0633522
cjross <cross@perq.com>
2015-01-30 16:49:55 -0500

    Add another test view

--- ch5/databases/lib/views-cjr.js
+++ ch5/databases/lib/views-cjr.js
@@ -1,5 +1,12 @@
 module.exports = {
 	"all": {
-		"map": "function(doc) { if (doc.Type == 'book')  emit(null, doc) }"
+		"map": "function(doc) { if (doc.title)  emit(null, doc) }"
+	},
+	all_authors: {
+		map: function(doc) {
+			if (doc._id && doc.title) {
+				emit(doc._id, doc.title);
+			}
+		}.toString()
 	}
 };
