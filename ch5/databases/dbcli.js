ea29c7d8f438cdd8f519b744b9df1a788e68bad7
Chris Ross <rosscj2533@gmail.com>
2015-01-31 14:22:07 -0500

    Resolve problems running my test views

--- ch5/databases/dbcli.js
+++ ch5/databases/dbcli.js
@@ -3,9 +3,12 @@ const
 	request = require('request'),
 	options = {
 		method: process.argv[2] || 'GET',
-		url: 'http://localhost:5984/' + (process.argv[3] || '')
+		url: 'http://localhost:5984/' + process.argv[3] || ''
 	};
-	
+// console.log('url = '+ options.url);
+// console.log('stringified url = '+ JSON.stringify(options.url));
+// console.log('argv = '+ process.argv[3]);
+// console.log('argv stringified = '+ JSON.stringify(process.argv[3]));
 request(options, function(err, res, body) {
 	if (err) {
 		throw Error(err);
